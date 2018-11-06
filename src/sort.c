6545c80d5fedf73d367ac6948e7f4b23ba17b87b
kemaqi <kemaqi@outlook.com>
2018-11-07 01:29:05 +0800

    add merge_sort

--- src/sort.c
+++ src/sort.c
@@ -49,6 +49,14 @@ void bubble_sort(int arr[],uint32_t len)
 		}
 	}
 }
+/*
+算法:快速排序
+输入参数:
+arr[]:需要排序的数组
+len  :排序数组的长度
+时间复杂度:O(log2 n)
+空间复杂度:O(1)
+*/
 void quick_sort(int arr[],uint32_t len)
 {
 	int i = 0;
@@ -82,6 +90,49 @@ void quick_sort(int arr[],uint32_t len)
 	quick_sort(arr+i+1,len-i-1);
 
 }
+void merge(int arr[],uint32_t start,uint32_t mid,uint32_t end)
+{
+	int *temp = (int *)malloc((end-start+1)*sizeof(int));
+	int i = start;
+	int j = mid+1;
+	int k = 0;
+	while(i<=mid && j<=end)
+	{
+		if(arr[i]<arr[j])
+		{
+			temp[k++] = arr[i++];
+		}
+		else
+		{
+			temp[k++] = arr[j++];
+		}
+	}
+	while(i<=mid)
+	{
+		temp[k++] = arr[i++];
+	}
+	while(j<=end)
+	{
+		temp[k++] = arr[j++];
+	}
+	for(int l=0;l<k;l++)
+	{
+		arr[start+l] = temp[l];
+	}
+	free(temp);
+
+}
+void merge_sort(int arr[],uint32_t start,uint32_t end)
+{
+	if(start>=end) return;
+
+	uint32_t mid = (start + end)/2;
+
+	merge_sort(arr,start,mid);
+	merge_sort(arr,mid+1,end);
+	merge(arr,start,mid,end);
+}
+
 bool test_sort(void)
 {
 	int arr[][6] = \
@@ -95,19 +146,19 @@ bool test_sort(void)
 
 	};
 
-	printf("&arr=%x\n", &arr);
-	printf("&arr+1=%x\n", &arr+1);
-	printf("arr=%x\n", arr);
-	printf("arr+1=%x\n", arr+1);
-	printf("&arr[0]=%x\n", &arr[0]);
-	printf("&arr[0]+1=%x\n", &arr[0]+1);
-	printf("arr[0]=%x\n", arr[0]);
-	printf("&arr[0][0]=%x\n", &arr[0][0]);
-	printf("&arr[0][0]+1=%x\n", &arr[0][0]+1);
-	printf("&arr[1]=%x\n", &arr[1]);
-	printf("arr[1]=%x\n", arr[1]);
-	printf("sizeof(arr)=%d\n", sizeof(arr));
-	printf("sizeof(arr[0])=%d\n", sizeof(arr[0]));
+	// printf("&arr=%x\n", &arr);
+	// printf("&arr+1=%x\n", &arr+1);
+	// printf("arr=%x\n", arr);
+	// printf("arr+1=%x\n", arr+1);
+	// printf("&arr[0]=%x\n", &arr[0]);
+	// printf("&arr[0]+1=%x\n", &arr[0]+1);
+	// printf("arr[0]=%x\n", arr[0]);
+	// printf("&arr[0][0]=%x\n", &arr[0][0]);
+	// printf("&arr[0][0]+1=%x\n", &arr[0][0]+1);
+	// printf("&arr[1]=%x\n", &arr[1]);
+	// printf("arr[1]=%x\n", arr[1]);
+	// printf("sizeof(arr)=%d\n", sizeof(arr));
+	// printf("sizeof(arr[0])=%d\n", sizeof(arr[0]));
 
 	for(int i=0;i<sizeof(arr)/sizeof(arr[0]) ;i++)
 	{
@@ -120,7 +171,8 @@ bool test_sort(void)
 	printf("%s\n","test start..." );
 	for(int i=0;i<sizeof(arr)/sizeof(arr[0]) ;i++)
 	{
-		quick_sort(arr[i],sizeof(arr[i])/sizeof(arr[i][0]));
+		//quick_sort(arr[i],sizeof(arr[i])/sizeof(arr[i][0]));
+		merge_sort(arr[i],0,sizeof(arr[i])/sizeof(arr[i][0])-1);
 	}
 	printf("%s\n","test end" );
 	for(int i=0;i<sizeof(arr)/sizeof(arr[0]) ;i++)
